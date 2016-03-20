2f0b4c51a181661329c6b63e8c98e9b0ec9ec049
EoghanMoylan <eoghanmoylan@hotmail.com>
2016-03-20 18:39:25 +0000

    Added more functionality, changed sets to lists

--- CountdownGame.py
+++ CountdownGame.py
@@ -2,13 +2,24 @@ words = open("wordlist.txt", "r")
 original = "education"
 annagrams = []
      
-other = set(original.rstrip())
-print(other)
+other = list(original.rstrip())
 	
+def compare(l1, l2):
+	isValid = True
+	for letter in l1:
+		if letter in l2:
+			l2.remove(letter)
+			#pass
+		elif len(l2)<=0:
+			isValid = True
+		else:
+			isValid = False
+	return isValid
+
 for word in words:
 	word = word.rstrip()
-	if len(word) == len(original):
-		if other == set(word):
+	if len(word) <= len(original):
+		if compare(other, list(word)) == True:
 			annagrams.append(word)
 		
 for annagram in annagrams:
