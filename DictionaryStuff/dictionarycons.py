b8eee5626c6dede5004e4f835d24e04d94283bfa
EoghanMoylan <eoghanmoylan@hotmail.com>
2016-03-28 17:35:24 +0100

    Modified script to exclude words over nine letters long

--- DictionaryStuff/dictionarycons.py
+++ DictionaryStuff/dictionarycons.py
@@ -3,9 +3,9 @@ morewords = open("wordlist2.txt", "r")
 new = open("wordlist.txt", "w")
 	
 for word in words:
-	if not word in morewords:
+	if not word in morewords and len(word) < 10:
 		new.write(word)
 		
 for word in morewords:
-	if not word in words:
+	if not word in words and len(word) < 10:
 		new.write(word)
